cc6f27a4da60623da0c1bb5d402cf8da18759be5
PalinV <palinv@mail.ru>
2024-07-16 17:10:51 +0300

    CRUD

---
+++ stocks_products/logistic/serializers.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+
+from logistic.models import Product, StockProduct, Stock
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    # настройте сериализатор для продукта
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    # настройте сериализатор для позиции продукта на складе
+    class Meta:
+        model = StockProduct
+        fields = ['id', 'quantity', 'price', 'product']
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+    class Meta:
+        model = Stock
+        fields = ['id', 'address', 'positions']
+    # настройте сериализатор для склада
+
+    def create(self, validated_data):
+        positions = validated_data.pop('positions')
+        stock = super().create(validated_data)
+        for position in positions:
+            print(f'Печать id product - "{position["product"].id}"')
+            StockProduct(quantity=position["quantity"], price=position["price"],
+                         stock_id=stock.pk, product_id=position["product"].id).save()
+        return stock
+
+    def update(self, instance, validated_data):
+        positions = validated_data.pop('positions')
+        stock = super().update(instance, validated_data)
+        for position in positions:
+            StockProduct.objects.filter(stock_id=stock.pk, product_id=position["product"].id)\
+                .update(
+                quantity=position["quantity"], price=position["price"],
+                )
+        return stock
+
